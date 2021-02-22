5aca63b44511d58e5e1a40f4e4644aa077d33929
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-22 17:26:37 +0200

    solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import {menuData} from '../data/MenuData'
+const Nav = styled.nav`
+    height:60px;
+    background:red;
+`
+const Logo = styled(Link)`
+    color:#fff
+`
+const MenuBars = styled.i`
+    
+`
 
+const NavMenu = styled.i`
+    
+`
+
+const NavMenuLinks = styled(Link)`
+    
+`
 const Navbar = () => {
     return (
-        <div>
+        <Nav>
+            <Logo to="/">BOWTIE</Logo>
+
+            <MenuBars/>
+            <NavMenu>
+                {menuData.map((item, index) => {
+                    <NavMenuLinks to={item.link} key={index}> 
+                    </NavMenuLinks>
+                })}
+            </NavMenu>
            <h1>Navbar</h1> 
-        </div>
+        </Nav>
     )
 }
 
