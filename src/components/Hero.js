5012eff0b2b99c02e7a5268723346af47e42e9d5
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-24 10:04:34 +0200

    slider buttons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { FaSlideshare } from 'react-icons/fa'
 import styled from 'styled-components'
 import { Button } from './Button'
-
+import {IoMdArrowRoundForward} from 'react-icons/io'
+import {IoArrowForward, IoArrowBack} from 'react-icons/io5'
 const HeroSection = styled.section`
     height:100vh;
     max-height: 1100px;
@@ -33,6 +34,66 @@ const HeroImage = styled.img`
 const HeroContent = styled.div`
     
 `
+const Arrow = styled(IoMdArrowRoundForward)`
+width:50px;
+height:50px;
+color:#fff;
+cursor:pointer;
+background:#000d1a;
+border-radius:50px;
+padding:10px;
+margin-right: 1rem;
+user-select:none;
+transition: 0.3s;
+
+&:hover{
+   background:#cd853f;
+   transform: scale(1.05); 
+}      
+`
+const PrevArrow = styled(IoArrowBack)`
+width:50px;
+height:50px;
+color:#fff;
+cursor:pointer;
+background:#000d1a;
+border-radius:50px;
+padding:10px;
+margin-right: 1rem;
+user-select:none;
+transition: 0.3s;
+
+&:hover{
+   background:#cd853f;
+   transform: scale(1.05); 
+}   
+`
+const NextArrow = styled(IoArrowForward)`
+width:50px;
+height:50px;
+color:#fff;
+cursor:pointer;
+background:#000d1a;
+border-radius:50px;
+padding:10px;
+margin-right: 1rem;
+user-select:none;
+transition: 0.3s;
+
+&:hover{
+   background:#cd853f;
+   transform: scale(1.05); 
+}  
+`
+
+
+const SliderButtons = styled.div`
+    position:absolute;
+    bottom:50px;
+    right: 50px;
+    display:flex;
+    z-index:10;
+`
 const Hero = ({slides}) => {
     return (
         <HeroSection>
@@ -49,14 +110,22 @@ const Hero = ({slides}) => {
                                 <HeroContent>
                                     <h1>{slide.title}</h1>
                                     <p>{slide.price}</p>
-                                    <Button to ={slide.path} primary='true'>
+                                    <Button to ={slide.path} primary='true' css={`max-width:160px;`}>
                                         {slide.label}
+                                        <Arrow />
                                     </Button>
                                 </HeroContent>
                             </HeroSlider>
                         </HeroSlide>
                     )
                 })}
+
+                <SliderButtons>
+                    <PrevArrow>
+
+                    </PrevArrow>
+                    <NextArrow></NextArrow>
+                </SliderButtons>
             </HeroWrapper>
         </HeroSection>
     )
