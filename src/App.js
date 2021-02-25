0624444ed864aca0ae29b7d9f98d383c75f087aa
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-25 12:40:32 +0200

    home page looking better

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import GlobalStyle from './globalStyles';
 import {SliderData} from './data/SliderData'
 import Dropdown from './components/Dropdown';
 import Footer from './components/Footer';
+import InfoSection from './components/InfoSection';
+import { InfoData } from './data/InfoData';
 
 function App() {
 
@@ -19,6 +21,7 @@ function App() {
     <Dropdown isOpen={isOpen} toggle={toggle}/>
     <Navbar toggle={toggle}/>   
     <Hero slides={SliderData} isOpen={isOpen} toggle={toggle}/>
+    <InfoSection {...InfoData} />
     <Footer/>
  </>
   );
---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior1.jpg'
+import ImageTwo from '../images/interior2.jpg'
+
+export const InfoData = {
+    heading: 'Explore our stunning properties',
+    paragraphOne: 'Situated in the South of Johannesburg, Bowtie Realty is dedicated to making your dreams a reality. Find the home that matches your vision of your future.',
+    paragraphTwo: 'Working together for success. The company Motto is Something we aim to embody in in every interaction with our clients. Free valuations and listings are some of the many benefits our clientell recieve. Become part of the list of satisfied buyers and sellers today.',
+    buttonLabel: 'View Homes',
+    image:ImageOne,
+    reverse:false,
+    delay:100
+};
+
+export const InfoDataTwo = {
+    heading: 'A Range of Designs',
+    paragraphOne: 'Together we can find your dream home. Our properties range from traditonal architecture to more modern designs that bring an artistic flare to your living space.',
+    paragraphTwo: "Work together with our interior designers and create the home you've envisioned",
+    buttonLabel: 'View Homes',
+    image:ImageTwo,
+    reverse:true,
+    delay:300
+};
